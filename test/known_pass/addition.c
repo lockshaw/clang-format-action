f189b1bca764f6dc5b3b540dc6dabd0640c32f4f
Colin Unger <lockshaw@lockshaw.net>
2023-08-08 01:22:20 -0700

    Fix formatting of test files

--- test/known_pass/addition.c
+++ test/known_pass/addition.c
@@ -1,17 +1,15 @@
 /* Addition program */
 #include <stdio.h>
 
-main()
-{
-	int integer1, integer2,
-		sum; /* declaration before any executable statements*/
+main() {
+  int integer1, integer2, sum; /* declaration before any executable statements*/
 
-	printf("Enter first integer\n"); /* prompt */
-	scanf("%d", &integer1); /* read an integer */
-	printf("Enter second integer\n"); /* prompt */
-	scanf("%d", &integer2); /* read an integer */
-	sum = integer1 + integer2; /* assignment of sum */
-	printf("Sum is %d\n", sum); /* print sum */
+  printf("Enter first integer\n");  /* prompt */
+  scanf("%d", &integer1);           /* read an integer */
+  printf("Enter second integer\n"); /* prompt */
+  scanf("%d", &integer2);           /* read an integer */
+  sum = integer1 + integer2;        /* assignment of sum */
+  printf("Sum is %d\n", sum);       /* print sum */
 
-	return 0; /* Indicate program ended successfully */
+  return 0; /* Indicate program ended successfully */
 }
